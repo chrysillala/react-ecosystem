572466ed781940168cb16616c49071c710caff56
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-09-20 20:48:45 +0700

    add loadTodos thunk

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,21 @@
+import { loadTodoInProgress, loadTodoSuccess, loadTodoFailure } from './actions';
+
+export const loadTodos = () => async (dispatch, getState) => {
+  // when loadTodos thunk is triggered, we want to dispatch loadTodoInProgress action
+
+  try {
+    dispatch(loadTodoInProgress());
+    const response = await fetch('http://localhost:8080/todos');
+    const todos = await response.json();
+
+    dispatch(loadTodoSuccess(todos));
+
+  } catch (error) {
+    dispatch(loadTodoFailure());
+    dispatch(displayAlert(error));
+  }
+}
+
 export const displayAlert = text => () => {
-  alert(`you click ${text}`);
+  alert(text);
 }
