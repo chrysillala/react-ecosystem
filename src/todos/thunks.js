249c099aafeb7e4d448c9e53a6ada98023142c43
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-09-20 23:03:56 +0700

    using thunk to create server resources

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,4 @@
-import { loadTodoInProgress, loadTodoSuccess, loadTodoFailure } from './actions';
+import { loadTodoInProgress, loadTodoSuccess, loadTodoFailure, createTodo } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
   // when loadTodos thunk is triggered, we want to dispatch loadTodoInProgress action
@@ -14,6 +14,25 @@ export const loadTodos = () => async (dispatch, getState) => {
     dispatch(loadTodoFailure());
     dispatch(displayAlert(error));
   }
+}
+
+export const addTodoRequest = (text) => async dispatch => {
+  try {
+    const body = JSON.stringify({ text });
+    const response = await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    });
+    const todo = await response.json();
+    dispatch(createTodo(todo));
+
+  } catch (error) {
+    dispatch(displayAlert(error));
+  }
+
 }
 
 export const displayAlert = text => () => {
