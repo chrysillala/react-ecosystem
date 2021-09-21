cced038f954c0dbada60dfb80b2f975e49b43a48
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-09-21 23:34:51 +0700

    incorporate isLoading state to todos state

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -7,56 +7,68 @@ import {
   LOAD_TODOS_FAILURE
 } from './actions';
 
-export const isLoading = (state = false, action) => {
-  // keep tracks of whether or not our todos are loading
-  const { type } = action;
-
-  switch (type) {
-    case LOAD_TODOS_IN_PROGRESS:
-      return true;
-    case LOAD_TODOS_SUCCESS:
-    case LOAD_TODOS_FAILURE:
-      return false;
-    default:
-      return state;
-  }
+const initialState = {
+  isLoading: false,
+  data: []
 }
 
-export const todos = (state = [], action) => {
+export const todos = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case CREATE_TODO: {
       const { todo } = payload;
-      return state.concat(todo);
+      return {
+        ...state,
+        data: state.data.concat(todo)
+      };
     }
 
     case REMOVE_TODO: {
       const { todo: todoToRemove } = payload; // rename `todo` to `todoToRemove`
-      return state.filter(todo => todo.id !== todoToRemove.id);
+      return {
+        ...state,
+        data: state.data.filter(todo => todo.id !== todoToRemove.id)
+      };
     }
 
     case MARK_TODO_AS_COMPLETED: {
       const { todo: updatedTodo } = payload;
-      return state.map(todo => {
-        if (todo.id === updatedTodo.id) {
-          return updatedTodo;
-        }
-        return todo;
-      });
+      return {
+        ...state,
+        data: state.data.map(todo => {
+          if (todo.id === updatedTodo.id) {
+            return updatedTodo;
+          }
+          return todo;
+        })
+      };
     }
 
     case LOAD_TODOS_SUCCESS: {
       // update todos state with the todos data from payload
       const { todos } = payload;
-      return todos;
+      return {
+        ...state,
+        isLoading: false,
+        data: todos
+      };
     }
 
+    case LOAD_TODOS_IN_PROGRESS:
+      return {
+        ...state,
+        isLoading: true
+      }
+
+    case LOAD_TODOS_FAILURE:
+      return {
+        ...state,
+        isLoading: false
+      }
     // remember to return the unchanged state if our reducer doesn't make any changes
     // since otherwise it'll look like our reducer is returning undefined
     // and it will throw an error
-    case LOAD_TODOS_IN_PROGRESS:
-    case LOAD_TODOS_FAILURE:
     default:
       return state;
   }
